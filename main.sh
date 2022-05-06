dc5dea7db5ef62193b553c0e5e441528dc13445a
Ameenbro123 <81590743+Ameenbro123@users.noreply.github.com>
2022-05-06 09:38:35 -0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,3 @@
 sudo gnome-terminal -x sh -c "sudo python3 /opt/BDFProxy/bdf_proxy.py"
+sudo gnome-terminal -x sh -c "msfconsole --resource scripts/resource.rc"
+sudo bettercap --interface eth0 -caplet scripts/caplet.cap
